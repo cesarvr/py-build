83bd7687e33e4144046dc3bc4ce4d5007f4d4819
cesar <cesar.valdez@gosub.consulting>
2020-06-05 09:25:36 +0100

    testing

--- smoke.py
+++ smoke.py
@@ -4,13 +4,18 @@ import urllib2
 
 url = sys.argv[1]
 
-print url
-response = urllib2.urlopen(url)
-html = response.read()
+print "Testing: ", url
 
-if html.find('indianred') == -1:
-    print "Fail: we expect indianred color in <H1>"
+try: 
+  response = urllib2.urlopen(url)
+  html = response.read()
+
+  if html.find('indianred') == -1:
+    print "Fail: we expect ** indianred ** color in <H1>"
     sys.exit(1)
-    
+except urllib2.URLError as e:
+  print "Fail:",  e.reason
+  sys.exit(1)
+
     
 print "Test Passing Successfully"
